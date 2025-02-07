ea83f318fbdef8f447e44d305acf847ec18b178f
Alan S. <lic.sanchezalan@gmail.com>
2025-02-06 22:23:18 -0300

    back deleted (& separated)

--- back/src/config.js
+++
@@ -1,7 +0,0 @@
-import dotenv from 'dotenv'
-
-dotenv.config();
-
-export const PORT = process.env.PORT
-export const MONGODB_URI = process.env.MONGODB_URI 
-export const SECRET = process.env.SECRET
--- back/src/utils/randomKeys.js
+++
@@ -1,4 +0,0 @@
-import crypto from 'crypto';
-
-const secret = crypto.randomBytes(64).toString('hex'); // Genera una cadena aleatoria de 64 bytes y la convierte a hexadecimal
-console.log(secret);
